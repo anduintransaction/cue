1ff0dbf27d238f5c97ce89f76941a6df8c6a1a0e
Tuan Vuong <vanhtuan0409@gmail.com>
2024-10-15 16:44:45 +0700

    feat: add authentication on custom registry

--- examples/anduin/multi-modules/cue.sh
+++ examples/anduin/multi-modules/cue.sh
@@ -4,6 +4,7 @@ here=`cd $(dirname $BASH_SOURCE); pwd`
 
 export ANDUIN_CUE_DEBUG="true"
 export CUE_CACHE_DIR=$here/cue.cache/
+export CUE_CONFIG_DIR=$here/cue.config/
 export CUE_REGISTRY=file://$here/registry.cue
 
-$here/../../../dist/cue-dev $@
+cue $@
